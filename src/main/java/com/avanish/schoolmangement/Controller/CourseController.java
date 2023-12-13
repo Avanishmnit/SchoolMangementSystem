e7413b471d8c61ecdd4d63a15f3ce7d184b4ad72
Avanishmnit <avanishmnit@gmail.com>
2023-12-13 16:29:03 +0530

    Intial Comment

--- src/main/java/com/avanish/schoolmangement/Controller/CourseController.java
+++ src/main/java/com/avanish/schoolmangement/Controller/CourseController.java
@@ -1,26 +1,18 @@
 package com.avanish.schoolmangement.Controller;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.avanish.schoolmangement.entities.Course;
+import com.avanish.schoolmangement.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.avanish.schoolmangement.entities.Course;
-import com.avanish.schoolmangement.services.CourseService;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
-	
+	//hi
 	@Autowired
 	private CourseService courseService;
 	
